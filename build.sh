af2968561d04e086ccd3077c7deb7d42db4bb9eb
LokiW <loki.w.white@gmail.com>
2018-07-05 11:44:27 -0700

    plugs into protoc now

--- build.sh
+++ build.sh
@@ -1,1 +1,13 @@
-g++ go_generator.cc go_plugin.cc -std=c++11 `pkg-config --cflags --libs protobuf` -lprotoc
+
+#build
+g++ go_generator.cc go_plugin.cc -std=c++11 `pkg-config --cflags --libs protobuf` -lprotoc -o protoc-gen-go_sapphire
+
+#install
+chmod +x protoc-gen-go_sapphire
+cp protoc-gen-go_sapphire /usr/local/bin/
+
+
+#run
+protoc --go_sapphire_out=output test.proto
+
+
